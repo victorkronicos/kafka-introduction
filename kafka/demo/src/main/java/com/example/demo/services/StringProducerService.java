ce7f40496ef9645b02664d1cdca4530682003850
Victor <victor_henrique@live.com>
2023-08-05 18:28:23 -0300

    feat: add callback function

--- kafka/demo/src/main/java/com/example/demo/services/StringProducerService.java
+++ kafka/demo/src/main/java/com/example/demo/services/StringProducerService.java
@@ -1,17 +1,43 @@
 package com.example.demo.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @RequiredArgsConstructor
+@Log4j2
 @Service
 public class StringProducerService {
 
+    @Qualifier("kafkaTemplate")
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+
+        future.whenComplete((result, ex) ->{
+            if (ex != null) {
+                log.error("Error sending message: {}", ex.getMessage());
+                return;
+            }
+
+            log.info("Message sent successfully: {}",
+                    result.getProducerRecord().value()
+            );
+
+            log.info(
+                    "Partition {}, Offset {}",
+                    result.getRecordMetadata().partition(),
+                    result.getRecordMetadata().offset()
+            );
+
+        });
+
     }
 }
