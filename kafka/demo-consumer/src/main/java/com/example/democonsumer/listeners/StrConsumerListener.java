6e59c43ad565320861d9e4098b225691515a4072
Victor <victor_henrique@live.com>
2023-08-12 17:50:52 -0300

    feat: implements of annotations

--- kafka/demo-consumer/src/main/java/com/example/democonsumer/listeners/StrConsumerListener.java
+++ kafka/demo-consumer/src/main/java/com/example/democonsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,29 @@
 package com.example.democonsumer.listeners;
 
+import com.example.democonsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message){
-       log.info("Receive message{}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message){
+       log.info("CREATE ::: message{}", message);
     }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message){
+        log.info("LOG ::: message{}", message);
+    }
+
+
+    @KafkaListener(groupId = "group-2", containerFactory = "validMessageContainerFactory")
+    public void history(String message){
+        log.info("HISTORY ::: message{}", message);
+    }
+
 }
