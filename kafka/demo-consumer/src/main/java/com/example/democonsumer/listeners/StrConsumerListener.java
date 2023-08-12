0cb3c1cdb5e6aba98a2d24a8c50f07a414be9b76
Victor <victor_henrique@live.com>
2023-08-12 18:13:05 -0300

    feat: custom error hander

--- kafka/demo-consumer/src/main/java/com/example/democonsumer/listeners/StrConsumerListener.java
+++ kafka/demo-consumer/src/main/java/com/example/democonsumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.example.democonsumer.listeners;
 
 import com.example.democonsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -10,20 +11,35 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
-       log.info("CREATE ::: message{}", message);
+        try{
+            log.info("CREATE ::: message{}", message);
+        } catch (Exception ex){
+            throw new IllegalArgumentException("EXCEPTION...");
+        }
+
     }
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
+        try{
         log.info("LOG ::: message{}", message);
+        } catch (Exception ex){
+            throw new IllegalArgumentException("EXCEPTION...");
+        }
     }
 
-
+    @SneakyThrows
     @KafkaListener(groupId = "group-2", topics = "str-topic" ,containerFactory = "validMessageContainerFactory")
     public void history(String message){
-        log.info("HISTORY ::: message{}", message);
+        try{
+            log.info("HISTORY ::: message{}", message);
+        } catch (Exception ex){
+            throw new IllegalArgumentException("EXCEPTION...");
+        }
     }
 
 }
